270b04c50ea294054e80df3ea779857604872202
Vinodkumar28 <rjvinod28@gmail.com>
2023-05-29 17:29:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+// import Compo1 from './Component';
+
+//class 1
+// function App() {
+
+//   let likes=100
+//   let image="path"
+//   return (
+//   <div>
+//     <h2>hi bro u have {likes} likes</h2>
+//     <img src={image}></img>
+//     <Compo1/>
+//   </div>
+//   );
+// }
+
+
+//class2
+function App() {
+  return (
+  <div className="App">
+      <h1>YouTube</h1>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Component.jsx
@@ -0,0 +1,7 @@
+//class 1
+// function Compo1(){
+//    return(
+//     <h1>welcome</h1>
+//    ) 
+// }
+// export default Compo1
